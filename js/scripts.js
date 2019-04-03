795862cbf4ee6f0ae46feddf2189f45932b6320b
faithngotho <faithwangari248@gmail.com>
2019-04-03 10:44:17 +0300

    Made changes to styles.css and scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 //user logic
 $(document).ready(function() {
   $("#submit").click(function(event) {
@@ -31,30 +31,3 @@ $(document).ready(function() {
 function refresh(){
   location.reload();
 }
-=======
-function check() {
-    var questionOne = document.Quiz.questionOne.value;
-    var questionTwo = document.Quiz.questionTwo.value;
-    var questionThree = document.Quiz.questionThree.value;
-    var questionFour = document.Quiz.questionFour.value
-    var right = 0;
-
-
-    if (questionOne == "correct") {
-        right = right + 25;
-    };
-    if (questionTwo == "correct") {
-        right = right + 25;
-    };
-    if (questionThree == "correct") {
-        right = right + 25;
-    };
-    if (questionFour == "correct") {
-        right = right + 25;
-
-    document.getElementById("fifth-p").style.visibility = "visible";
-    document.getElementById("correctNumber").innerHTML = "HERE IS YOUR SCORE: " + right + "%";
-    $(".container").fadeOut();
-
-};
->>>>>>> c60be27e41103c856a34c57933bd0bd184f5eb44
